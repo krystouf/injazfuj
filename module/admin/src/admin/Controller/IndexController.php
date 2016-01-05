b1cb459619aa1088882e9b0f27d547d3c47e99f9
Administrator <Administrator@TECHqudah>
2016-01-05 08:48:31 +0400

    attendance

--- module/admin/src/admin/Controller/IndexController.php
+++ module/admin/src/admin/Controller/IndexController.php
@@ -9,9 +9,14 @@
 
 namespace admin\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
-use Zend\View\Model\ViewModel;
 
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Db\Sql\Sql;
+use Zend\View\Model\ViewModel;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+use Zend\Db\Sql\Select;
+use Zend\Session\Container;
 class IndexController extends AbstractActionController
 {
     public function indexAction()
@@ -21,7 +26,33 @@ class IndexController extends AbstractActionController
     
     public function attendanceAction()
     {
-        return new ViewModel();
+        $sm =$this->getServiceLocator();
+        $dbAdpater = $sm->get('Zend\Db\Adapter\Adapter'); 
+        $name ="Qudah";
+        $sql="";
+     //   $resultSet = $adapter->query($sql, \Zend\Db\Adapter\Adapter::QUERY_MODE_EXECUTE);
+        $sql = "SELECT * FROM attendance ";
+        $resultSet = $dbAdpater->query($sql);
+        $resultSet->execute();
+  
+         $att = $resultSet->toArray();
+                foreach ($att as $attrow){
+                 $name =$attrow['St_Id'];
+                }
+        /*
+           $tablegetaway = new TableGateway('attendance', $dbAdpater);
+           $rowset = $tablegetaway->select(function(Select $select){
+      //     $select->where(array('Abs_period'=> 2 ));
+                });
+                $att = $rowset->toArray();
+                foreach ($att as $attrow){
+                 $name =$attrow['St_Id'];
+                }
+         * 
+         */
+        return new ViewModel(array(
+            'name'=>$name,
+        ));
     }
 
 }
